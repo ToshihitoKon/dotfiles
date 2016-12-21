620d807de5aa79f609ee6549400e6564212af816
ToshihitoKon <shiratori991@gmail.com>
2016-12-21 17:53:11 +0900

    フルスクリーンにできるようになった

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,8 @@ import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Gaps
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.ToggleLayouts
 
 --XMonad.Hooks
 import XMonad.Hooks.DynamicLog
@@ -21,14 +23,16 @@ main = do
             { borderWidth       = 1
             , terminal          = "urxvt"
             , modMask           = modm
-            , layoutHook        = myLayoutHooks
+            , layoutHook        = toggleLayouts Full  myLayoutHooks
             }
           `additionalKeys`
           [ ((modm, xK_a), sendMessage MirrorExpand)
           , ((modm, xK_z), sendMessage MirrorShrink)
+          , ((modm .|. controlMask, xK_space), sendMessage ToggleLayout)
           ]
         )
 
 
 myLayoutHooks = do
-    avoidStruts $ gaps [(U,8),(D,8),(L,8),(R,8)] $ spacing 3 $ ResizableTall 1 (2/50) (2/3) []
+    avoidStruts $ gaps [(U,8),(D,8),(L,8),(R,8)] $ spacing 3 $
+                  ResizableTall 1 (2/50) (2/3) []
